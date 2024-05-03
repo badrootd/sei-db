52b27f16081e63673d5fc6bc16d92dcaa40fe826
a <a>
2024-05-04 00:58:25 +0500

    Fix paths

--- common/utils/path.go
+++ common/utils/path.go
@@ -3,11 +3,11 @@ package utils
 import "path/filepath"
 
 func GetCommitStorePath(homePath string) string {
-	return filepath.Join(homePath, "data", "committer.db")
+	return filepath.Join(homePath, "committer.db")
 }
 
 func GetStateStorePath(homePath string, backend string) string {
-	return filepath.Join(homePath, "data", backend)
+	return filepath.Join(homePath, backend)
 }
 
 func GetChangelogPath(commitStorePath string) string {
